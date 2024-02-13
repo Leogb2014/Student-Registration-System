7e3e8d931f4e3c39e46d2c039d6b93a9caa26e45
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-02-13 11:48:13 -0300

    Class ResourceNotFoundException

---
+++ src/main/java/com/Leonardo/studentRegistration/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.Leonardo.studentRegistration.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Long id) {
+		super("ID: " + id + "not found");
+	}
+
+}
