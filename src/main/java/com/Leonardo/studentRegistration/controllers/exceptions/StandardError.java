3377fa6b724e041a4fd61402d7cabd65688b1541
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-02-13 12:00:50 -0300

    Class StandardError

---
+++ src/main/java/com/Leonardo/studentRegistration/controllers/exceptions/StandardError.java
@@ -0,0 +1,68 @@
+package com.Leonardo.studentRegistration.controllers.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Instant timeStamp;
+	private Integer status;
+	private String message;
+	private String error;
+	private String path;
+
+	public StandardError() {
+
+	}
+
+	public StandardError(Instant timeStamp, Integer status, String message, String error, String path) {
+		super();
+		this.timeStamp = timeStamp;
+		this.status = status;
+		this.message = message;
+		this.error = error;
+		this.path = path;
+	}
+
+	public Instant getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Instant timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
